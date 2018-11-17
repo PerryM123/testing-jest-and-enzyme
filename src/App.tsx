f9f1272662bb53e0eaa1d6f3d10e5324846e5941
Perry M <perry.miranda@Perrys-MacBook-Air.local>
2018-11-17 16:19:04 +0900

    Typescriptを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-class App extends Component {
+export default class App extends React.Component {
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={'abc'} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -24,5 +25,3 @@ class App extends Component {
     );
   }
 }
-
-export default App;
